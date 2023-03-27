dcdc24372c58b4649baf9649adc7688af980eef1
Zhang Zhe <zzhe@amazon.com>
2023-03-27 13:34:33 +0000

    Init repo

---
+++ dingtalk/eventHandler/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"runtime/debug"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/sirupsen/logrus"
+)
+
+type Text struct {
+	Content string `json:"content"`
+}
+
+type Markdown struct {
+	Title string `json:"title"`
+	Text  string `json:"text"`
+}
+
+type OapiRobotSendRequest struct {
+	MsgType  string   `json:"msgtype"`
+	Text     Text     `json:"text,omitempty"`
+	Markdown Markdown `json:"markdown,omitempty"`
+}
+
+type OapiRobotSendResponse struct {
+	Errcode int64  `json:"errcode"`
+	Errmsg  string `json:"errmsg"`
+}
+
+type HealthEvent struct {
+	Version    string   `json:"version,omitempty"`
+	ID         string   `json:"id,omitempty"`
+	DetailType string   `json:"detail-type,omitempty"`
+	Source     string   `json:"source,omitempty"`
+	Account    string   `json:"account,omitempty"`
+	Time       string   `json:"time,omitempty"`
+	Region     string   `json:"region,omitempty"`
+	Resources  []string `json:"resources,omitempty"`
+	Detail     struct {
+		Arn               string `json:"arn,omitempty"`
+		Service           string `json:"service,omitempty"`
+		EventTypeCode     string `json:"eventTypeCode,omitempty"`
+		EventTypeCategory string `json:"eventTypeCategory,omitempty"`
+		Region            string `json:"region,omitempty"`
+		StartTime         string `json:"startTime,omitempty"`
+		EndTime           string `json:"endTime,omitempty"`
+		LastUpdatedTime   string `json:"lastUpdatedTime,omitempty"`
+		StatusCode        string `json:"statusCode,omitempty"`
+		EventScopeCode    string `json:"eventScopeCode,omitempty"`
+	} `json:"detail,omitempty"`
+}
+
+type EC2InstanceStageChangeEvent struct {
+	Account string `json:"account,omitempty"`
+	Detail  struct {
+		Instance_Id string `json:"instance-id,omitempty"`
+		State       string `json:"state,omitempty"`
+	} `json:"detail,omitempty"`
+	Detail_Type string   `json:"detail-type,omitempty"`
+	ID          string   `json:"id,omitempty"`
+	Region      string   `json:"region,omitempty"`
+	Resources   []string `json:"resources,omitempty"`
+	Source      string   `json:"source,omitempty"`
+	Time        string   `json:"time,omitempty"`
+	Version     string   `json:"version,omitempty"`
+}
+
+func GetSecretValue(ctx context.Context) (string, string, error) {
+
+	secretKey := os.Getenv("BOT_SECRET_KEY")
+
+	if secretKey == "" {
+		logrus.Errorf("bot secret key is not set in environment variable: BOT_SECRET_KEY")
+	}
+	secretARN := os.Getenv("WEBHOOK_SECRET_ARN")
+
+	if secretARN == "" {
+		logrus.Errorf("webhook secret ARN of bot is not set in environment variable: WEBHOOK_SECRET_ARN")
+	}
+
+	cfg, err := config.LoadDefaultConfig(ctx)
+
+	if err != nil {
+		logrus.Errorf("failed to load AWS config: %w", err)
+	}
+
+	svc := secretsmanager.NewFromConfig(cfg)
+
+	output, err := svc.GetSecretValue(ctx, &secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(secretARN),
+	})
+
+	if err != nil {
+		logrus.Errorf("failed to get secret value: %w", err)
+	}
+
+	secretValue := aws.ToString(output.SecretString)
+	if secretValue == "" {
+		logrus.Errorf("secret value is empty")
+	}
+	return secretValue, secretKey, nil
+}
+
+func HandleRequest(ctx context.Context, snsEvent events.SNSEvent) error {
+
+	defer func() {
+		if r := recover(); r != nil {
+			logrus.Infof("panic is %v", string(debug.Stack()))
+		}
+	}()
+
+	snsMsg := snsEvent.Records[0].SNS.Message
+
+	var healthevent HealthEvent
+
+	err := json.Unmarshal([]byte(snsMsg), &healthevent)
+
+	if err != nil {
+		logrus.Errorf("%w", err)
+	}
+
+	secretValue, secretKey, _ := GetSecretValue(ctx)
+
+	req := OapiRobotSendRequest{
+		MsgType: "markdown",
+		Markdown: Markdown{
+			Title: secretKey,
+			//			Text:  "# AWS Health Event Notification\n --- \n\n#### **Detail type:**\n" + healthevent.DetailType + "\n#### AWS **账号**",
+			Text: snsMsg,
+		},
+	}
+
+	jsonReq, err := json.Marshal(req)
+	if err != nil {
+		return fmt.Errorf("error encoding JSON: %v", err)
+	}
+
+	logrus.Infof(string(jsonReq))
+
+	httpReq, err := http.NewRequest("POST", secretValue, bytes.NewBuffer(jsonReq))
+	if err != nil {
+		return fmt.Errorf("error creating HTTP request: %v", err)
+	}
+	httpReq.Header.Set("Content-Type", "application/json")
+
+	client := http.DefaultClient
+	resp, err := client.Do(httpReq)
+	if err != nil {
+		return fmt.Errorf("error sending HTTP request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	var jsonResp OapiRobotSendResponse
+	err = json.NewDecoder(resp.Body).Decode(&jsonResp)
+	if err != nil {
+		return fmt.Errorf("error decoding JSON: %v", err)
+	}
+
+	if jsonResp.Errcode != 0 {
+		return fmt.Errorf("error sending message: %d %s", jsonResp.Errcode, jsonResp.Errmsg)
+	}
+
+	return nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
